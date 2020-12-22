6e6890ba73b142073edde90364b463c30f992f69
Manuel <manuel.devito@icloud.com>
2020-12-22 22:09:09 +0100

    Esercizio

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,54 @@
+<footer>
+    <div class="contenitore">
+        <div class="contenitore-footer">
+            <div class="contenitore-indirizzi">
+                <img src="{{ asset('images/la-molisana-logo.png')}}" alt="La Molisana - Logo">
+                <ul>
+                    <li>Ragione sociale: La Molisana S.P.A.</li>
+                    <li>Sede legale: Contrada Colle Delle Alpi, 100/A</li>
+                    <li>86100 - Campobasso (CB)</li>
+                    <li>Pec: lamolisana@pec.it</li>
+                    <li>TEL: +39 0874 4981</li>
+                    <li>FAX: +39 0874 629584</li>
+                    <li>info@lamolisana.it (per segnalazione degli utenti)</li>
+                    <li>commerciale@lamolisana.it</li>
+                    <li>export@lamolisana.it</li>
+                    <li>numero verde 800818081</li>
+                    <li>telefono 3801292455</li>
+                </ul>
+            </div>
+            <div class="contenitore-pastificio">
+                <h2>Panificio</h2>
+                <ul>
+                    <li>Il Pastificio</li>
+                    <li>Grano decorticato a pietra</li>
+                    <li>Il Molise c’è</li>
+                    <li>Filiera Integrata</li>
+                    <li>100 anni di pasta</li>
+                    <li>Sartoria della pasta</li>
+                    <li>Spaghetto Quadrato</li>
+                    <li>Le Persone</li>
+                </ul>
+                <h2 class="title-prodotti">Prodotti</h2>
+                <ul>
+                    <li>Le Classiche</li>
+                    <li>Le Integrali</li>
+                    <li>Le Speciali</li>
+                    <li>Le Biologiche</li>
+                    <li>Le Gluten-Free</li>
+                    <li>Le Semole</li>
+                    <li>Le Extra di Lusso</li>
+                </ul>
+            </div>
+            <div class="contenitore-chef">
+                <h2>Collezione da chef</h2>
+                <ul>
+                    <li>Collezione da Chef</li>
+                    <li>Grandi Cucine</li>
+                    <li>Biologiche</li>
+                    <li>Quadrate</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -3,7 +3,40 @@
 @section('titolo', 'Prodotti - La Molisana')
 
 @section('contenuto')
-
-<h1>N.38 Mezze Maniche</h1>
-
+    <main>
+        <div class="contenitore">
+            <div class="contenitore-pasta">
+                <h1>Le Lunghe</h1>
+                <div class="card-contenitore">
+                    @foreach ($formati as $formato)
+                        @if ($formato['tipo'] == 'lunga')
+                            <div class="card">
+                                <img src="{{ $formato['src'] }}" alt="Pasta Lunga">
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+                <h1>Le corte</h1>
+                <div class="card-contenitore">
+                    @foreach ($formati as $formato)
+                        @if ($formato['tipo'] == 'corta')
+                            <div class="card">
+                                <img src="{{ $formato['src'] }}" alt="Pasta Corta">
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+                <h1>Le cortissime</h1>
+                <div class="card-contenitore">
+                    @foreach ($formati as $formato)
+                        @if ($formato['tipo'] == 'cortissima')
+                            <div class="card">
+                                <img src="{{ $formato['src'] }}" alt="Pasta Cortissima">
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </main>
 @endsection
