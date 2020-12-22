cf538191d54cc6a5f31ebeed04e68b549b61e833
Manuel <manuel.devito@icloud.com>
2020-12-22 22:38:29 +0100

    Miglioramenti Esercizio

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,9 @@
 <footer>
     <div class="contenitore">
         <div class="contenitore-footer">
-            <div class="contenitore-indirizzi">
+            <div class="lunghezza contenitore-indirizzi">
                 <img src="{{ asset('images/la-molisana-logo.png')}}" alt="La Molisana - Logo">
-                <ul>
+                <ul class="lista">
                     <li>Ragione sociale: La Molisana S.P.A.</li>
                     <li>Sede legale: Contrada Colle Delle Alpi, 100/A</li>
                     <li>86100 - Campobasso (CB)</li>
@@ -17,9 +17,9 @@
                     <li>telefono 3801292455</li>
                 </ul>
             </div>
-            <div class="contenitore-pastificio">
+            <div class="lunghezza contenitore-pastificio">
                 <h2>Panificio</h2>
-                <ul>
+                <ul class="lista">
                     <li>Il Pastificio</li>
                     <li>Grano decorticato a pietra</li>
                     <li>Il Molise c’è</li>
@@ -30,7 +30,7 @@
                     <li>Le Persone</li>
                 </ul>
                 <h2 class="title-prodotti">Prodotti</h2>
-                <ul>
+                <ul class="lista">
                     <li>Le Classiche</li>
                     <li>Le Integrali</li>
                     <li>Le Speciali</li>
@@ -40,9 +40,9 @@
                     <li>Le Extra di Lusso</li>
                 </ul>
             </div>
-            <div class="contenitore-chef">
+            <div class="lunghezza contenitore-chef">
                 <h2>Collezione da chef</h2>
-                <ul>
+                <ul class="lista">
                     <li>Collezione da Chef</li>
                     <li>Grandi Cucine</li>
                     <li>Biologiche</li>
